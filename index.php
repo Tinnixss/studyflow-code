ef77ec9875b2981f4a3c223db957518f640cf3ca
Tinnixss <valentina.paixao@estudante.ifto.edu.br>
2026-06-02 15:59:35 -0300

    melhoria index

--- index.php
+++ index.php
@@ -3,13 +3,7 @@
 // 1. Liga o carregador automático inteligente do seu projeto
 require_once __DIR__ . '/autoload.php';
 
-// 2. CAMINHOS MANUAIS REAIS (Apenas para o que o Autoload não mapeia ou tem nome diferente)
-require_once __DIR__ . '/app/model/Database.php';                 // <-- Carrega o seu Database real!
-require_once __DIR__ . '/app/repository/IEstudanteRepository.php'; // Interface do repositório
-require_once __DIR__ . '/app/services/service.php';               // Seu arquivo de serviço real
-require_once __DIR__ . '/app/controllers/controller.php';         // Seu arquivo de controlador real
-
-// 3. Monta a aplicação na ordem correta de dependências
+// 2. Monta a aplicação na ordem correta (O Autoload vai achar todo mundo sozinho agora!)
 $repository = new EstudanteRepository();
 $service = new StudyFlowService($repository);
 $controller = new EstudanteController($service);
