f2935140331058dff31266f2dfd4d79f24acbce1
Tinnixss <valentina.paixao@estudante.ifto.edu.br>
2026-06-02 16:02:42 -0300

    melhoria index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 // 1. Liga o carregador automático inteligente do seu projeto
 require_once __DIR__ . '/autoload.php';
 
-// 2. Monta a aplicação na ordem correta (O Autoload vai achar todo mundo sozinho agora!)
+// 2. Monta a aplicação na ordem correta
 $repository = new EstudanteRepository();
 $service = new StudyFlowService($repository);
-$controller = new EstudanteController($service);
+$controller = new EstudanteController($service); 
 
 // Passamos o controlador pronto para o Router
 $router = new Router($controller);
