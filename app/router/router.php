7c522809eed22992fc081dc249b967f8de4e73ee
Tinnixss <valentina.paixao@estudante.ifto.edu.br>
2026-06-02 15:55:26 -0300

    melhoria router

--- app/router/router.php
+++ app/router/router.php
@@ -1,13 +1,5 @@
 <?php
 
-// Descobre a pasta raiz 'app' dinamicamente
-$raizApp = dirname(__DIR__);
-
-// Importa os arquivos usando caminhos absolutos baseados na pasta 'app'
-require_once $raizApp . '/repository/IEstudanteRepository.php';
-require_once $raizApp . '/repository/EstudanteRepository.php';
-require_once $raizApp . '/controllers/EstudanteController.php';
-
 class Router {
     private EstudanteController $controller;
 
@@ -31,11 +23,11 @@ class Router {
         }
 
         // Caminho exato até a sua view
-        $viewPath = $raizApp . '/view/php/view.php';
+        $viewPath = dirname(__DIR__) . '/view/php/view.php';
         if (file_exists($viewPath)) {
             require_once $viewPath;
         } else {
-            require_once $raizApp . '/view/html/index.html';
+            require_once dirname(__DIR__) . '/view/html/index.html';
         }
     }
 }
